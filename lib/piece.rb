4b950ee18cde5e1fdacb2fa1bad77be2f439e980
Anna Dolan and Lee Saville <anna.dolan@gmail.com>
2017-05-05 15:47:34 -0600

    Add increment and decrement to Piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,28 @@
 class Piece
   attr_reader :color
-  
+
   def initialize(color)
     @color = color
+  end
+
+  def horizontal_increment(starting_position)
+    "#{starting_position[0].next}#{starting_position[1]}"
+    #new_pos if in_bounds?(new_pos)
+  end
+
+  def horizontal_decrement(starting_position)
+    "#{starting_position[0].ord.pred.chr}#{starting_position[1]}"
+  end
+
+  def vertical_increment(starting_position)
+    "#{starting_position[0]}#{starting_position[1].next}"
+  end
+
+  def vertical_decrement(starting_position)
+    "#{starting_position[0]}#{starting_position[1].to_i.pred.to_s}"
+  end
+
+  def in_bounds?(position)
+    raise "Stop that!" if !(position.chars[0].between?('a', 'h') && position.chars[1].between('1', '8'))
   end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -8,4 +8,38 @@ RSpec.describe Piece do
       expect(p).to respond_to(:color)
     end
   end
+
+  describe 'movements' do
+    context '.horizontal_increment' do
+      it 'increments horizontally' do
+        piece = Piece.new('black')
+
+        expect(piece.horizontal_increment('a1')).to eq('b1')
+      end
+    end
+
+    context '.horizontal_decrement' do
+      it 'decrements horizontally' do
+        piece = Piece.new('black')
+
+        expect(piece.horizontal_decrement('b1')).to eq('a1')
+      end
+    end
+
+    context '.vertical_increment' do
+      it 'increments vertically' do
+        piece = Piece.new('black')
+
+        expect(piece.vertical_increment('a1')).to eq('a2')
+      end
+    end
+
+    context '.vertical_decrement' do
+      it 'decrements vertically' do
+        piece = Piece.new('black')
+
+        expect(piece.vertical_decrement('a2')).to eq('a1')
+      end
+    end
+  end
 end
