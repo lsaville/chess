faa959d5e7f2b46f88d27be421070d562b3920a7
Jon Kimble and Lee Saville <l.w.saville@gmail.com>
2017-05-20 15:49:43 -0600

    Change tests to expect another parameter indicating number of spaces to check

--- spec/move_calculator_spec.rb
+++ spec/move_calculator_spec.rb
@@ -2,26 +2,30 @@ require_relative '../lib/move_calculator'
 
 RSpec.describe MoveCalculator do
   context '.horizontal_increment' do
-    it 'increments horizontally' do
-      expect(MoveCalculator.horizontal_increment('a1')).to eq('b1')
+    it 'increments horizontally by 1' do
+      expect(MoveCalculator.horizontal_increment('a1', 1)).to eq('b1')
+    end
+
+    it 'increments horizontally by 2' do
+      expect(MoveCalculator.horizontal_increment('a1', 2)).to eq('c1')
     end
   end
 
   context '.horizontal_decrement' do
     it 'decrements horizontally' do
-      expect(MoveCalculator.horizontal_decrement('b1')).to eq('a1')
+      expect(MoveCalculator.horizontal_decrement('b1', 2)).to eq('a1')
     end
   end
 
   context '.vertical_increment' do
     it 'increments vertically' do
-      expect(MoveCalculator.vertical_increment('a1')).to eq('a2')
+      expect(MoveCalculator.vertical_increment('a1', 2)).to eq('a2')
     end
   end
 
   context '.vertical_decrement' do
     it 'decrements vertically' do
-      expect(MoveCalculator.vertical_decrement('a2')).to eq('a1')
+      expect(MoveCalculator.vertical_decrement('a2', 2)).to eq('a1')
     end
   end
 end
