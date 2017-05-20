62a8922a55111400236c91cef49d7bdb6836f63d
Jon Kimble and Lee Saville <l.w.saville@gmail.com>
2017-05-20 15:50:17 -0600

    Change the calculator to accept a parameter indicating number of spaces

--- lib/move_calculator.rb
+++ lib/move_calculator.rb
@@ -1,19 +1,23 @@
 module MoveCalculator
   #need to change these methods so that we can supply a multiplier and move multiple spaces
   class << self
-    def horizontal_increment(starting_position)
-      "#{starting_position[0].next}#{starting_position[1]}"
+    def horizontal_increment(starting_position, num_spaces)
+      new_position = (starting_position[0].ord + num_spaces).chr
+      "#{new_position}#{starting_position[1]}"
     end
 
-    def horizontal_decrement(starting_position)
+    def horizontal_decrement(starting_position, num_spaces)
+      new_position = (starting_position[0].ord + num_spaces).chr
       "#{starting_position[0].ord.pred.chr}#{starting_position[1]}"
     end
 
-    def vertical_increment(starting_position)
+    def vertical_increment(starting_position, num_spaces)
+      new_position = (starting_position[1].ord + num_spaces).chr
       "#{starting_position[0]}#{starting_position[1].next}"
     end
 
-    def vertical_decrement(starting_position)
+    def vertical_decrement(starting_position, num_spaces)
+      new_position = (starting_position[1].ord + num_spaces).chr
       "#{starting_position[0]}#{starting_position[1].to_i.pred.to_s}"
     end
   end
